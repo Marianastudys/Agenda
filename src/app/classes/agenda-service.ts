e19a8e1429061e7f2c5baba3f66503d8137f9b0a
ruthyinfom <ruthyanne.priscila@academico.ifrn.edu.br>
2026-06-22 16:01:14 -0300

    1 ok e exibe-contatos criado

--- src/app/classes/agenda-service.ts
+++ src/app/classes/agenda-service.ts
@@ -35,4 +35,8 @@ export class AgendaService {
     }
      return false;
   }
+
+  obterTodos(): Contato[] {
+    return this.#contatos;
+  }
 }
--- src/app/exibe-contatos/exibe-contatos.ts
+++ src/app/exibe-contatos/exibe-contatos.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AgendaService } from '../classes/agenda-service';
+import { Contato } from '../classes/contato';
 
 @Component({
   selector: 'app-exibe-contatos',
@@ -6,4 +8,11 @@ import { Component } from '@angular/core';
   templateUrl: './exibe-contatos.html',
   styleUrl: './exibe-contatos.scss',
 })
-export class ExibeContatos {}
+export class ExibeContatos {
+    #agendaService = inject(AgendaService)
+    protected readonly contatos: Contato[] = []
+
+    constructor() {
+      this.contatos = this.#agendaService.obterTodos()
+    }
+}
