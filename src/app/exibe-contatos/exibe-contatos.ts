25521f6f686182b7aeaf910956a4c4a13b74eef6
ruthyinfom <ruthyanne.priscila@academico.ifrn.edu.br>
2026-06-22 15:53:20 -0300

    exibe-contatos

---
+++ src/app/exibe-contatos/exibe-contatos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exibe-contatos',
+  imports: [],
+  templateUrl: './exibe-contatos.html',
+  styleUrl: './exibe-contatos.scss',
+})
+export class ExibeContatos {}
